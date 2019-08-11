45ed8432ef348add29ea2b83a5438663f7307cf6
Steve Kelly <kd2cca@gmail.com>
2019-08-11 10:33:22 -0400

    add MT to the mix

--- src/MeshingBenchmarks.jl
+++ src/MeshingBenchmarks.jl
@@ -10,8 +10,11 @@ using BenchmarkTools
 function benchmark()
     here = dirname(@__FILE__)
 
+    println(pwd())
     ctacardio = load(here*"/../data/CTA-cardio.nrrd")
     q = -100
+
+    samples = 1
 
     # flip sign on nrrd data
     # TODO Meshing should support this
@@ -23,17 +26,28 @@ function benchmark()
     # Meshing might want to support this format natively
     ctasdf = SignedDistanceField(HyperRectangle(Vec(0,0,0), Vec(10,10,10)),ctacardio.data)
     println("CTA-cardio.nrrd loaded")
-    println("CTA-cardio.nrrd Float32 runtime")
-    m = @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($ctasdf, MarchingCubes($q))
-    for i in 1:5
-        m = @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($ctasdf, MarchingCubes($q))
+
+    println("CTA-cardio.nrrd MarchingCubes Float32 runtime")
+    mc = @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($ctasdf, MarchingCubes($q))
+    for i in 1:samples-1
+        @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($ctasdf, MarchingCubes($q))
     end
-    println("CTA-cardio.nrrd Float64 runtime")
-    m = @btime HomogenousMesh{Point{3,Float64},Face{3,Int}}($ctasdf, MarchingCubes($q))
-    for i in 1:5
-        m = @btime HomogenousMesh{Point{3,Float64},Face{3,Int}}($ctasdf, MarchingCubes($q))
+
+    println("CTA-cardio.nrrd MarchingCubes Float64 runtime")
+    @btime HomogenousMesh{Point{3,Float64},Face{3,Int}}($ctasdf, MarchingCubes($q))
+    for i in 1:samples-1
+        @btime HomogenousMesh{Point{3,Float64},Face{3,Int}}($ctasdf, MarchingCubes($q))
     end
-    save("ctacardio.ply", m)
+
+    println("CTA-cardio.nrrd MarchingTetrahedra Float32 runtime")
+    mt = @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($ctasdf, MarchingTetrahedra($q))
+    for i in 1:samples-1
+        @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($ctasdf, MarchingTetrahedra($q))
+    end
+
+    println("Saving files")
+    save("ctacardio_mc.ply", mc)
+    save("ctacardio_mt.ply", mt)
 end
 
 end # module
