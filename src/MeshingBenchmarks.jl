4fb44d6d2876fcd1ab2220751d0b5b0c43616de3
Steve Kelly <kd2cca@gmail.com>
2019-09-08 22:14:16 -0400

    possible api updates, may revert later, no regressions, performance or otherwise

--- src/MeshingBenchmarks.jl
+++ src/MeshingBenchmarks.jl
@@ -12,42 +12,52 @@ function benchmark()
 
     println(pwd())
     ctacardio = load(here*"/../data/CTA-cardio.nrrd")
-    q = -100
+    q = 100
 
     samples = 1
 
-    # flip sign on nrrd data
-    # TODO Meshing should support this
-    for i in eachindex(ctacardio.data)
-        ctacardio.data[i] = -ctacardio.data[i]
-    end
-
-    # convert axis array to sdf
-    # Meshing might want to support this format natively
-    ctasdf = SignedDistanceField(HyperRectangle(Vec(0,0,0), Vec(10,10,10)),ctacardio.data)
     println("CTA-cardio.nrrd loaded")
 
     println("CTA-cardio.nrrd MarchingCubes Float32 runtime")
-    mc = @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($ctasdf, MarchingCubes($q))
+    mc = @btime HomogenousMesh(isosurface($ctacardio, MarchingCubes(iso=$q, insidepositive=true), Point{3,Float32}, Face{3,Int})...)
     for i in 1:samples-1
-        @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($ctasdf, MarchingCubes($q))
+        @btime HomogenousMesh(isosurface($ctacardio, MarchingCubes(iso=$q, insidepositive=true), Point{3,Float32}, Face{3,Int})...)
     end
 
     println("CTA-cardio.nrrd MarchingCubes Float64 runtime")
-    @btime HomogenousMesh{Point{3,Float64},Face{3,Int}}($ctasdf, MarchingCubes($q))
+    @btime HomogenousMesh(isosurface($ctacardio, MarchingCubes(iso=$q, insidepositive=true), Point{3,Float64}, Face{3,Int})...)
     for i in 1:samples-1
-        @btime HomogenousMesh{Point{3,Float64},Face{3,Int}}($ctasdf, MarchingCubes($q))
+        @btime HomogenousMesh(isosurface($ctacardio, MarchingCubes(iso=$q, insidepositive=true), Point{3,Float64}, Face{3,Int})...)
     end
 
-    println("CTA-cardio.nrrd MarchingTetrahedra Float32 runtime")
-    mt = @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($ctasdf, MarchingTetrahedra($q))
-    for i in 1:samples-1
-        @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($ctasdf, MarchingTetrahedra($q))
-    end
+    # println("CTA-cardio.nrrd MarchingTetrahedra Float32 runtime")
+    # mt = @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($ctasdf, MarchingTetrahedra($q))
+    # for i in 1:samples-1
+    #     @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($ctasdf, MarchingTetrahedra($q))
+    # end
+
+
 
     println("Saving files")
     save("ctacardio_mc.ply", mc)
-    save("ctacardio_mt.ply", mt)
+    #save("ctacardio_mt.ply", mt)
+end
+
+function brain(samples=1, q=100)
+
+    here = dirname(@__FILE__)
+
+    brain = load(here*"/../Seg3DData/Brain_DataSet/MRI-brain50.nrrd")
+
+    brainsdf = SignedDistanceField(HyperRectangle(Vec(0,0,0), Vec(10,10,10)),brain.data)
+
+    println("Brain.nrrd MarchingCubes Float32 runtime")
+    mc_brain = @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($brainsdf, MarchingCubes(iso=$q, insidepositive=false))
+    for i in 1:samples-1
+        @btime HomogenousMesh{Point{3,Float32},Face{3,Int}}($brainsdf, MarchingCubes(iso=$q, insidepositive=false))
+    end
+
+    save("brian_mc.ply", mc_brain)
 end
 
 end # module
